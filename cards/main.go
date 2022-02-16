1a6c759434870179a2629cd94bd2a8d3aa93351b
Jonathan Cristiano da Silva <p22.jonathan@webmotors.com.br>
2022-02-16 11:27:21 -0300

    New Deck from File and Shuffle

--- cards/main.go
+++ cards/main.go
@@ -3,15 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-	// hand, remainingDeck := deal(cards, 5)
-	// hand.print()
-	// remainingDeck.print()
-
 	cards := newDeck()
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
 
-	err := cards.saveToFile("my_cards")
+	cardsToFile := newDeck()
+	err := cardsToFile.saveToFile("my_cards")
 	if err != nil {
 		fmt.Println("Error saving file", err)
 	}
+
+	cardsFromFile := newDeckFromFile("my_cards")
+	cardsFromFile.print()
+
+	cardsShuffle := newDeck()
+	cardsShuffle.shuffle()
+	cardsShuffle.print()
 }
